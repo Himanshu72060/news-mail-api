ba9e185ae0956ef52bb2cb8dc09ee7c846c3a0db
Himanshu72060 <himanshusharma00898@gmail.com>
2025-06-13 15:57:33 +0530

    ok

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const postRoutes = require('./routes/postRoutes');
@@ -6,6 +7,9 @@ const postRoutes = require('./routes/postRoutes');
 dotenv.config();
 
 const app = express();
+
+app.use(cors());
+
 app.use(express.json());
 
 app.use('/api', postRoutes);
